360d7d746d7473cecf1a9fdbd7b2e8b459160efb
OsadaInduwara <74148791+OsadaInduwara@users.noreply.github.com>
2021-08-11 11:48:08 +0530

    Update userdata.php

--- bizlogic/userdata.php
+++ bizlogic/userdata.php
@@ -1,10 +1,5 @@
 <?php
 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Max-Age: 1000");
-    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-    header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE"); 
 
     include("./connection.php");
 
